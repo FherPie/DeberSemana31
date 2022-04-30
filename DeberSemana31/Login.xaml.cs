9d3c2b0f297898199d29b08a1fa7094b952ccbad
Andres Piedra <andres.piedra@gizlocorp.com>
2022-04-29 19:58:49 -0500

    Agregar archivos de proyecto.

---
+++ DeberSemana31/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeberSemana31
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtusuario.Text;
+            string contrasena = txtcontrasena.Text;
+            if (usuario=="estudiante2021" && contrasena=="uisrael2021") {
+                await Navigation.PushAsync(new Principal(usuario, contrasena));
+            }
+            else
+            {
+                await DisplayAlert("ERROR", "Credenciales Incorrectas", "Aceptar");
+
+            }
+            
+        }
+    }
+}
---
+++ DeberSemana31/Principal.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeberSemana31
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Principal : ContentPage
+    {
+        private string usuario;
+        private string contrasena;
+
+        public Principal()
+        {
+           
+        }
+
+        public Principal(string usuario, string contrasena)
+        { 
+
+            InitializeComponent();
+            this.usuario=usuario;
+            this.contrasena=contrasena;
+            this.txtBienvenida.Text= "Bienvenido: "+usuario;
+           
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double txtseguimiento1value = Double.Parse(txtseguimiento1.Text);
+
+            double txtexamenvalue = Double.Parse(txtExamen.Text);
+
+            txtNotaParcial1.Text=  (txtseguimiento1value*0.3 + txtexamenvalue*0.2).ToString();
+
+
+            double txtseguimiento2Value = Double.Parse(txtseguimiento2.Text);
+
+            double txtExamenFValue = Double.Parse(txtExamenF.Text);
+
+            txtNotaParcial2.Text=  (txtseguimiento2Value*0.3 + txtExamenFValue*0.2).ToString();
+
+
+            notaFinal.Text= (Double.Parse(txtNotaParcial2.Text)+ Double.Parse(txtNotaParcial1.Text)).ToString();
+
+
+
+            if (Double.Parse(notaFinal.Text)>=7.0)
+            {
+                DisplayAlert("Mensaje", "Aprobado", "Aceptar");
+
+            }
+            else if (Double.Parse(notaFinal.Text)>=5 && Double.Parse(notaFinal.Text)<=6.9)
+            {
+                DisplayAlert("Mensaje", "Complementario", "Aceptar");
+
+            }
+            else if (Double.Parse(notaFinal.Text)<5)
+            {
+                DisplayAlert("Mensaje", "Reprobado", "Aceptar");
+
+            }
+        }
+    }
+}
